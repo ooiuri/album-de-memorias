1a991769d65e6d3b164c3da204e7cc8599ca5c62
Glitch (presentao) <none>
2020-06-10 19:30:19 +0000

    🍘🙎 Updated with Glitch

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,66 +1,78 @@
-class flower{
-    constructor(xPos, yPos, rad, numLeafs, speed, scalef){
-       // this.xPos = this.xPos +this.rad/2;
-        this.rad = rad;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.numLeafs = numLeafs;
-        this.speed = speed * scalef;
-        this.scalef = scalef;
-        console.log('x: '+this.xPos +' y:'+ this.yPos +' v:'+ this.speed + ' scale:'+ this.scalef);
-    }
-    show() {
-        
-        colorMode('hsl'); 
-        scale(this.scalef);
-    fill(0,159,90);
+class flower {
+  constructor(xPos, yPos, rad, numLeafs, speed, scalef) {
+    // this.xPos = this.xPos +this.rad/2;
+    this.rad = rad;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.numLeafs = numLeafs;
+    this.speed = speed * scalef;
+    this.speedX=this.speed;
+    this.scalef = scalef;
+    //console.log("x: " + this.xPos + " y:" + this.yPos + " v:" +this.speed +" scale:" +this.scalef);
+  }
+  show() {
+    push();
+    colorMode("RGB");
+    scale(this.scalef);
+    fill(0, 159, 90);
     noFill();
     strokeWeight(3);
-    stroke('white')
-    for(var i=0;i<this.numLeafs;i++){
-            push();
-            translate(this.xPos,this.yPos);
-            rotate(this.yPos*.01+radians(i*360/this.numLeafs));
-            arc(110,-30,140,140,radians(30),radians(180-30),CHORD);
-            arc(110,-30+140/2,140,140,radians(180+30),radians(-30),CHORD);
-            pop();
-        }
-        fill('white');
-        ellipseMode(CENTER);  // Set ellipseMode to CORNERS
-        noStroke();
-        ellipse(this.xPos,this.yPos,140);
-        scale(1/this.scalef);
+    stroke("white");
+    for (var i = 0; i < this.numLeafs; i++) {
+      push();
+      translate(this.xPos, this.yPos);
+      rotate(this.yPos * 0.01 + radians((i * 360) / this.numLeafs));
+      arc(110, -30, 140, 140, radians(30), radians(180 - 30), CHORD);
+      arc(110, -30 + 140 / 2, 140, 140, radians(180 + 30), radians(-30), CHORD);
+      pop();
     }
+    fill("white");
+    ellipseMode('CENTER'); // Set ellipseMode to Center
+    noStroke();
+    ellipse(this.xPos, this.yPos, 140);
+    //scale(1 / this.scalef);
+    pop();
+  }
 
-    move() {
-        this.yPos -= this.speed;
-        this.xPos = this.xPos + 2*sin(this.yPos/random(30,50))+ (mouseX-width/2)*.1;
-        if(this.xPos < 0 - 2*this.rad) this.xPos = windowWidth * 1/this.scalef;
-        if(this.xPos >  windowWidth * 1/this.scalef) this.xPos = 0;
-        if(this.yPos < 0 - 2*this.rad ) this.yPos = windowHeight * 1/this.scalef;
-        lastmousex = mouseX;
-      }
-
+  move() {
+    this.yPos -= this.speed;
+    this.speedX =(mouseX - width / 2) * this.scalef * .1;
+    //this.speedX*=0.9
+    this.xPos += this.speedX;
+    if (this.xPos < 0 - 2 * this.rad)
+      this.xPos = (windowWidth * 1) / this.scalef;
+    if (this.xPos > (windowWidth * 1) / this.scalef) this.xPos = 0;
+    if (this.yPos < 0 - 2 * this.rad)
+      this.yPos = (windowHeight * 1) / this.scalef;
+    lastmousex = mouseX;
+  }
 }
 
-
-class textreveal{
-    constructor(texto, x, y){
+class textreveal {
+  constructor(texto, x, y,size = 18) {
+    push()
     this.sourceText = texto;
     this.x = x;
     this.y = y;
-    textSize(18);
-    color('white');
-    this.reveal = 0
-    }
-    show(){
-        if (this.reveal < this.sourceText.length) this.reveal += .5;
-        text(this.sourceText.substring(0, this.reveal), this.x, this.y);
-    }
-    unshow(){
-        if (this.reveal > 0) this.reveal -= .5;
-        text(this.sourceText.substring(0, this.reveal), this.x, this.y);
-    }
+    this.size = size;
+    color("white");
+    this.reveal = 0;
+    pop()
+  }
+  show() {
+    push();
+    textSize(this.size);
+    if (this.reveal < this.sourceText.length) this.reveal += 0.5;
+    text(this.sourceText.substring(0, this.reveal), this.x, this.y);
+    pop();
+  }
+  unshow() {
+    push();
+    textSize(this.size);
+    if (this.reveal > 0) this.reveal -= 0.5;
+    text(this.sourceText.substring(0, this.reveal), this.x, this.y);
+    pop();
+  }
 }
 /*
 let reveal = 0
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 /* código principal */
-var numero_flores = 5;
+var numero_flores = 10;
 let flor = [numero_flores]; //objeto flor que fica no background
 var lastmousex;
 var scribble; //retangulo desenhado a mao
-
 
 var pag = 0; // variável controle de caminho
 var luz = true;
@@ -14,49 +13,50 @@ let texto;
 let bg1;
 
 function preload() {
-  bg1 = loadImage('assets/bg1.jpg');
+  bg1 = loadImage("assets/bg1.jpg");
 }
 
-
-function setup(){
-    createCanvas(windowWidth, windowHeight); //criando canva
-    background('black');
-    lastmousex = mouseX;
-    scribble = new Scribble();
-    textSize(18);
-    //Criando as flores
-    for(i = 0; i < numero_flores; i++){
-        flor[i] = new flower(random(windowWidth)/.2, random(windowHeight)/.2, 80, 11, 10, random(.05,.2)); //padão
-    }
-    texto = new textreveal('clique para continuar',width/2,height/1.5);
+function setup() {
+  createCanvas(windowWidth, windowHeight); //criando canva
+  background("black");
+  lastmousex = mouseX;
+  scribble = new Scribble();
+  textSize(18);
+  //Criando as flores
+  for (i = 0; i < numero_flores; i++) {
+    flor[i] = new flower(
+      random(windowWidth) / 0.2,    //xPos
+      random(windowHeight) / 0.2,   //yPos
+      80,  //rad
+      11,  //numLeafs
+      10,   //speed
+      random(0.05, 0.2) //scalef
+    ); 
+  }
+  texto = new textreveal("clique para continuar", width / 2, height / 1.5);
 }
 
-function draw(){
-    //background(0);
-    inicio(pag < 3);
+function draw() {
+  //background(0);
+  inicio(pag < 3,pag);
 }
-
-
-
 
 //reconhece cliques do mouse
 function mousePressed() {
-    pag = pag + 1;
-    if(pag > 3) pag = 0;
+  pag = pag + 1;
+  if (pag > 3) pag = 0;
 }
 
 //botoes
-function keyPressed(){
-   
-    if(key == 'l'){
-        luz = true;
-    }else if(key == 'k'){
-        luz = false;
-    }
+function keyPressed() {
+  if (key == "l") {
+    luz = true;
+  } else if (key == "k") {
+    luz = false;
+  }
 }
 
 //redimensiona a página
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
-
+  resizeCanvas(windowWidth, windowHeight);
+}
